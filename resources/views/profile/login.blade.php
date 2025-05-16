47bfb1e444d822dd178329b6723525e0abc22f26
Luka Barth <luka.costa@db1.com.br>
2025-05-16 15:04:07 -0300

    refactor(view): added class to style

--- resources/views/profile/login.blade.php
+++ resources/views/profile/login.blade.php
@@ -6,15 +6,17 @@
 <h2>Login</h2>
 
 <!-- action="{{ route('login') }}" action definida dentro do arquivo "web.php" -->
-<form method="POST" action="{{ route('login') }}">
-    @csrf
+<div class="register-form">
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
 
-    <label for="email">Email:</label><br>
-    <input type="email" name="email" id="email" required><br><br>
+        <label for="email">Email:</label><br>
+        <input type="email" name="email" id="email" required><br><br>
 
-    <label for="password">Senha:</label><br>
-    <input type="password" name="password" id="password" required><br><br>
+        <label for="password">Senha:</label><br>
+        <input type="password" name="password" id="password" required><br><br>
 
-    <button type="submit">Entrar</button>
-</form>
+        <button class="button-submit" type="submit">Entrar</button>
+    </form>
+</div>
 @endsection
