9d19f8e599ea90377354c6e68e4c5d1c61f82c22
Luka Barth <luka.costa@db1.com.br>
2025-05-19 16:06:18 -0300

    test: Fix logout test by mocking session forget method

--- tests/Unit/LogoutUserServiceTest.php
+++ tests/Unit/LogoutUserServiceTest.php
@@ -4,19 +4,25 @@ namespace Tests\Unit;
 
 use App\Http\Services\LogoutUserService;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Mockery;
 use PHPUnit\Framework\TestCase;
 
 class LogoutUserServiceTest extends TestCase
 {
+    public function tearDown(): void
+    {
+        Mockery::close();
+    }
+
     public function testItLogsOutUserAndInvalidatesSession(): void
     {
-        Auth::shouldReceive('logout')->once();
+        $session = Mockery::mock();
+        $session->shouldReceive('forget')->with('user_id')->once();
+        $session->shouldReceive('invalidate')->once();
+        $session->shouldReceive('regenerateToken')->once();
 
         $request = Mockery::mock(Request::class);
-        $request->shouldReceive('session->invalidate')->once();
-        $request->shouldReceive('session->regenerateToken')->once();
+        $request->shouldReceive('session')->andReturn($session);
 
         $service = new LogoutUserService();
         $service->execute($request);
