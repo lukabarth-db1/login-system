dd699840eba22deca63128011b3754c4f49e9a33
Luka Barth <luka.costa@db1.com.br>
2025-05-22 10:33:04 -0300

    refactor(service): no longer use laravel method for obtain user session

--- app/helpers.php
+++ app/helpers.php
@@ -1,12 +1,23 @@
 <?php
 
+use App\Http\Services\TokenService;
 use App\Models\User;
 
-function user()
+function user(): ?User
 {
-    if (session()->has('user_id')) {
-        return User::find(session('user_id'));
+    if (!isset($_COOKIE['custom_session_id'])) {
+        return null;
     }
 
-    return null;
+    $sessionId = $_COOKIE['custom_session_id'];
+
+    $token = new TokenService();
+
+    $sessionData = $token->getSessionData($sessionId);
+
+    if (!$sessionData || !isset($sessionData['user_id'])) {
+        return null;
+    }
+
+    return User::find($sessionData['user_id']);
 }
